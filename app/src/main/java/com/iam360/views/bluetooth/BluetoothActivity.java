77e240e42f3b37a10763911e65fe4adcdb05b8ac
Lotti <charlotte.proeller@gmx.de>
2017-04-05 15:17:56 +0200

    added fragment to view

--- app/src/main/java/com/iam360/views/bluetooth/BluetoothActivity.java
+++ app/src/main/java/com/iam360/views/bluetooth/BluetoothActivity.java
@@ -11,5 +11,18 @@ public class BluetoothActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            // Create a new Fragment to be placed in the activity layout
+            BluetoothConnectionFragment bluetoothFrag = new BluetoothConnectionFragment();
+
+            bluetoothFrag.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, bluetoothFrag).commit();
+        }
     }
+
 }
